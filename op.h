1db57ade5e081677e039bd707b4b27257683f5e4
Master76 <15811297278@163.com>
2019-05-22 00:03:11 +0800

    fix multi definition link error

--- op.h
+++ op.h
@@ -5,29 +5,29 @@ typedef int (*binaryOp)(int, int);
 
 namespace operators {
 // unary operators
-int unaryAdd(int val)
+inline int unaryAdd(int val)
 {
   return val;
 }
-int unarySub(int val)
+inline int unarySub(int val)
 {
   return -val;
 }
 
 // binary operators
-int binaryAdd(int left, int right)
+inline int binaryAdd(int left, int right)
 {
   return left + right;
 }
-int binarySub(int left, int right)
+inline int binarySub(int left, int right)
 {
   return left - right;
 }
-int binaryMul(int left, int right)
+inline int binaryMul(int left, int right)
 {
   return left * right;
 }
-int binaryDiv(int left, int right)
+inline int binaryDiv(int left, int right)
 {
   return left / right;
 }
